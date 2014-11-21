c6bbfc6823acd65697f6509f422deba3993cb652
Pierre PLAZANET <plazanet.pierre@gmail.com>
2014-11-21 19:13:16 +0100

    Delete test-suite.php

--- bin/test-suite.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include __DIR__ . '/../vendor/autoload.php';
-
-$input = new Symfony\Component\Console\Input\ArgvInput($argv);
-$application = new Knp\Async\Application;
-$application->run($input);
