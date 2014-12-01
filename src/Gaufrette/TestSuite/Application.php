f664f640012a2f4ecd3507d3298cfd4ab5901c76
Pierre PLAZANET <plazanet.pierre@gmail.com>
2014-12-01 17:52:33 +0100

    Remove default Helpers

--- src/Gaufrette/TestSuite/Application.php
+++ src/Gaufrette/TestSuite/Application.php
@@ -4,6 +4,7 @@ namespace Gaufrette\TestSuite;
 
 use Gaufrette\TestSuite\Command\RunCommand;
 use Symfony\Component\Console\Application as BaseApplication;
+use Symfony\Component\Console\Helper\HelperSet;
 use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\ConsoleOutput;
@@ -21,5 +22,10 @@ class Application extends BaseApplication
         $this->configureIO($input, $output);
 
         parent::run($input, $output);
+    }
+
+    protected function getDefaultHelperSet()
+    {
+        return new HelperSet(array());
     }
 }
